cc68ed34e424ce5bd953eef3e2058faa10500ca1
yaogengzhu <455947455@qq.com>
2021-09-29 10:30:53 +0800

    husky生效

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './component/App.jsx'
 
-
-
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 console.log(process.env, 'process.env')
