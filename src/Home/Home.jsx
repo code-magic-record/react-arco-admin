6496777fce003f2ca1e45c992aea656312ed2a9e
yaogengzhu <455947455@qq.com>
2021-10-08 15:58:36 +0800

    test

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
 import './index.less';
 
 const Home = () => {
-  return <div className="home-page">Home</div>;
+  return (
+    <div className="home-page">
+      <Button color="primary">按钮</Button>
+      <Button color="danger">按钮</Button>
+    </div>
+  );
 };
 
 export default Home;
