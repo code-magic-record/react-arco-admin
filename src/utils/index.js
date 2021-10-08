97ca2b23b60838ab929df2918e0fbf950a1b6135
yaogengzhu <455947455@qq.com>
2021-10-08 10:18:22 +0800

    优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,6 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-10-06 17:06:57
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2021-10-06 17:07:33
+ * @Last Modified time: 2021-10-08 10:11:57
  * 工具函数
  */
--- template/js/index.js
+++ template/js/index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+/*
+ * @Author: yaogeng.zhu
+ * @Date: 2021-10-08 10:17:01
+ * @Last Modified by: yaogeng.zhu
+ * @Last Modified time: 2021-10-08 10:17:44
+ *  存放一些其他的js脚本
+ */
