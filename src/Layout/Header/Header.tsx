3a7fd5b085d7577f30504afb09d1a0370a6f6b0f
yaogengzhu <455947455@qq.com>
2022-02-01 14:00:06 +0800

    feature: 头像部分处理

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,4 +1,6 @@
-import { Avatar, Layout } from '@arco-design/web-react';
+import { Avatar, Dropdown, Layout, Menu } from '@arco-design/web-react';
+import { IconPoweroff, IconSettings } from '@arco-design/web-react/icon';
+
 import React from 'react';
 import './index.less';
 
@@ -9,11 +11,25 @@ const Header = () => {
     <Layout.Header>
       <div className={classPrefix}>
         <div className={`${classPrefix}-avatar`}>
-          <Avatar
-            autoFixFontSize={false}
+          <Dropdown
+            position="br"
+            droplist={
+              <Menu>
+                <Menu.Item key="1">
+                  <IconSettings />
+                  <span className="ml-[5px]">用户设置</span>
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <IconPoweroff />
+                  <span className="ml-[5px]">退出登录</span>
+                </Menu.Item>
+              </Menu>
+            }
           >
-            <img src="https://avatars.githubusercontent.com/u/42566669?v=4" alt="" />
-          </Avatar>
+            <Avatar autoFixFontSize={false}>
+              <img src="https://avatars.githubusercontent.com/u/42566669?v=4" alt="" />
+            </Avatar>
+          </Dropdown>
         </div>
       </div>
     </Layout.Header>
