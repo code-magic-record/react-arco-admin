2e37c1ff14341c8de1fb8d5b1f812932f4ef3b5d
yaogengzhu <455947455@qq.com>
2022-02-17 15:13:34 +0800

    feat： 新增全屏模式

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Dropdown, Layout, Menu } from '@arco-design/web-react';
-import { IconPoweroff, IconSettings } from '@arco-design/web-react/icon';
+import { IconFullscreen, IconFullscreenExit, IconPoweroff, IconSettings } from '@arco-design/web-react/icon';
 import { useNavigate } from 'react-router-dom';
+import { useFullscreen } from 'ahooks';
 
 import React from 'react';
 import './index.less';
@@ -8,7 +9,7 @@ import './index.less';
 const classPrefix = 'header';
 const Header = () => {
   const navigate = useNavigate();
-
+  const [fullscreen, { toggleFullscreen }] = useFullscreen(() => document.getElementById('root'));
   const loginOut = () => {
     navigate('/login');
   };
@@ -17,11 +18,23 @@ const Header = () => {
     navigate('/');
   };
 
+  const fullscreenEvent = () => {
+    toggleFullscreen();
+  };
+
   return (
     <Layout.Header>
       <div className={classPrefix}>
         <div className={`${classPrefix}-logo`} onClick={goHome}>
           <h1 style={{ margin: 0, marginLeft: '10px' }}>React Arco Admin</h1>
+        </div>
+        {/* 全屏 */}
+        <div className={`${classPrefix}-fullscreen`} onClick={fullscreenEvent}>
+          {!fullscreen ? (
+            <IconFullscreen style={{ fontSize: '28px' }} />
+          ) : (
+            <IconFullscreenExit style={{ fontSize: '28px' }} />
+          )}
         </div>
         <div className={`${classPrefix}-avatar`}>
           <Dropdown
