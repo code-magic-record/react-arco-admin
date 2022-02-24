1cd35c44c8b01e50145a4590673012d56cc3222b
yaogengzhu <455947455@qq.com>
2022-02-24 16:04:04 +0800

    feat: 工作台基础结构调整

--- src/app/Dashboard/WorkPlace/WorkPlace.tsx
+++ src/app/Dashboard/WorkPlace/WorkPlace.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './index.less';
+
+const classPrefix = 'workpalce';
 
 const Workplace = () => {
   return (
-    <div>
-      <h1>Workplace</h1>
+    <div className={`${classPrefix}`}>
+      <div className={`${classPrefix}-left`}>
+        <div className={`${classPrefix}-left-top`}></div>
+        <div className={`${classPrefix}-left-bottom`}></div>
+      </div>
+      <div className={`${classPrefix}-right`}></div>
     </div>
   );
 };
