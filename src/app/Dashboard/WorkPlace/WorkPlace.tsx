73038d7791765f10009ef5bb13e9699a4354fb32
yaogengzhu <455947455@qq.com>
2022-03-07 13:02:56 +0800

    fix:  test 自动化构建

--- src/app/Dashboard/WorkPlace/WorkPlace.tsx
+++ src/app/Dashboard/WorkPlace/WorkPlace.tsx
@@ -1,13 +1,13 @@
 import { Card, Grid } from '@arco-design/web-react';
 import React from 'react';
 import styles from './index.module.less';
-import Banner from './modules/slide/banner'
+import Banner from './modules/slide/banner';
 import ShortCuts from './modules/slide/Shortcuts';
 
 const { Row, Col } = Grid;
 
 const Workplace = () => {
-  console.log(styles, '....')
+  console.log(styles, '....');
   return (
     <div className={styles.workplace}>
       <Row style={{ marginBottom: 20, height: '100%' }}>
@@ -18,15 +18,9 @@ const Workplace = () => {
           </Card>
           <Row gutter={24} style={{ height: '100%' }}>
             <Col span={8} push={16}>
-              <Card bordered={false} style={{ width: '100%' }}>
-                Card content
-              </Card>
+              <Card bordered={false} style={{ width: '100%' }}></Card>
             </Col>
-            <Col span={16} pull={8}>
-              <Card bordered={false} style={{ width: '100%' }}>
-                Card content
-              </Card>
-            </Col>
+            <Col span={16} pull={8}></Col>
           </Row>
         </Col>
         {/* 右侧布局 */}
