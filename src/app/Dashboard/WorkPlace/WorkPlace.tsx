429dbc6ecc5d4979488aac41b47f03e1eaeb8bf2
yaogengzhu <455947455@qq.com>
2022-03-13 14:40:11 +0800

    feat: add page

--- src/app/Dashboard/WorkPlace/WorkPlace.tsx
+++ src/app/Dashboard/WorkPlace/WorkPlace.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid } from '@arco-design/web-react';
+import { Grid } from '@arco-design/web-react';
 import React from 'react';
 import styles from './index.module.less';
 import Banner from './modules/slide/banner';
@@ -11,17 +11,9 @@ const Workplace = () => {
   return (
     <div className={styles.workplace}>
       <Row style={{ marginBottom: 20, height: '100%' }}>
-        {/* 左侧布局 */}
-        <Col flex="auto" style={{ height: '100%' }}>
+        <Col flex="auto">
           <Overview />
-          <Row gutter={24} style={{ height: '100%' }}>
-            <Col span={8} push={16}>
-              <Card bordered={false} style={{ width: '100%' }}></Card>
-            </Col>
-            <Col span={16} pull={8}></Col>
-          </Row>
         </Col>
-        {/* 右侧布局 */}
         <Col flex="280px" style={{ height: '100%', marginLeft: '20px' }}>
           <ShortCuts />
           <Banner />
