b8ddfb5b23121950ab2e82500426f9c5a4d8ca11
yaogengzhu <455947455@qq.com>
2022-03-14 17:26:14 +0800

    fix: 解决react18、createRoot引用报错问题

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './App';
 import './mock'
