71404f432b2deaff23d519889a39bb517a07e307
yaogengzhu <455947455@qq.com>
2022-03-14 22:36:15 +0800

    进度条颜色

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,10 @@
 import { generate, getRgbStr } from '@arco-design/color';
 import { v4 as uuidv4 } from 'uuid';
 
+/**
+ * 修改主题色
+ * @param newColor
+ */
 export function changePageColor(newColor: string) {
   const newList = generate(newColor, {
     list: true,
@@ -9,6 +13,7 @@ export function changePageColor(newColor: string) {
     const rgbStr = getRgbStr(l);
     document.body.style.setProperty(`--arcoblue-${index + 1}`, rgbStr);
   });
+  document.body.style.setProperty('--them_color', newColor)
 }
 
 export function uuid() {
