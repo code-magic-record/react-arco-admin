c7bdfa17a39747c072adffcd26639c1de6b8d8de
yaogengzhu <455947455@qq.com>
2022-03-16 16:23:58 +0800

    feat: login页面支持国际化&less重构

---
+++ src/app/Login/locales/en-us.ts
@@ -0,0 +1,12 @@
+const en: {
+  [k: string]: string;
+} = {
+  'login.forgetPassword': 'forget password',
+  'login.rememberPassword': 'remember password',
+  'login.login': 'login',
+  'login.registerAccount': 'register account',
+  'login.username.isNotEmpty': 'the username cannot be empty',
+  'login.password.isNotEmpty': 'the password cannot be empty',
+};
+
+export default en;
---
+++ src/app/Login/locales/zh-cn.ts
@@ -0,0 +1,12 @@
+const zh: {
+  [k: string]: string;
+} = {
+  'login.forgetPassword': '忘记密码',
+  'login.rememberPassword': '记住密码',
+  'login.login': '登录',
+  'login.registerAccount': '注册账号',
+  'login.username.isNotEmpty': '用户名不能为空',
+  'login.password.isNotEmpty': '密码不能为空'
+};
+
+export default zh;
