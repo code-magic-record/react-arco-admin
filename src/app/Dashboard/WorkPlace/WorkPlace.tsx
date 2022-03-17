d4e1ce7ffdfb41e81b1b435e875b10ea0ecd5c14
yaogengzhu <455947455@qq.com>
2022-03-17 22:40:33 +0800

    feat: add page

--- src/app/Dashboard/WorkPlace/WorkPlace.tsx
+++ src/app/Dashboard/WorkPlace/WorkPlace.tsx
@@ -2,6 +2,7 @@ import { Grid } from '@arco-design/web-react';
 import React from 'react';
 import styles from './index.module.less';
 import Banner from './modules/slide/banner';
+import ContentView from './modules/slide/contentView'
 import Overview from './modules/slide/overview';
 import ShortCuts from './modules/slide/shortcuts';
 
@@ -13,6 +14,7 @@ const Workplace = () => {
       <Row style={{ marginBottom: 20, height: '100%' }}>
         <Col flex="auto" style={{ width: '0px' }}>
           <Overview />
+          <ContentView />
         </Col>
         <Col flex="280px" style={{ height: '100%', marginLeft: '20px' }}>
           <ShortCuts />
---
+++ src/app/Dashboard/WorkPlace/modules/config/index.ts
@@ -0,0 +1,18 @@
+type IhaderList = Array<{
+  id: number;
+  title: string;
+}>;
+export const haderList: IhaderList = [
+  {
+    id: 1,
+    title: '文本',
+  },
+  {
+    id: 2,
+    title: '图文',
+  },
+  {
+    id: 3,
+    title: '视频',
+  },
+];
