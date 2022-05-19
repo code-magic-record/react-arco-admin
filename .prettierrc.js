42d75860a2d4d32f6f1775cfac7c2da55d1ae34c
yaogengzhu <455947455@qq.com>
2022-05-19 18:17:01 +0800

    chore: update doc

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  quoteProps: 'preserve',
+  arrowParens: 'avoid',
+  overrides: [
+    {
+      'files': ['*.md'],
+      'options': {
+        embeddedLanguageFormatting: 'off',
+      },
+    },
+  ],
+}
