63dd5daf99e5312fb31632d5e38876f56b4c4f27
nmsn <136696700@qq.com>
2022-07-13 21:24:19 +0800

    feat(result): 新增 Result 相关页面

---
+++ src/app/Result/locales/en-us.ts
@@ -0,0 +1,19 @@
+const en: {
+  [k: string]: string
+} = {
+  'menu.result': 'Result page',
+
+  'menu.result.success': 'success',
+  'menu.result.success.title': 'Success message',
+  'menu.result.success.subTitle': 'This is a success description.',
+  'menu.result.success.again': 'Again',
+  'menu.result.success.back': 'Back',
+
+  'menu.result.error': 'fail',
+  'menu.result.error.title': 'Error message',
+  'menu.result.error.subTitle': 'Something went wrong. Please try again.',
+  'menu.result.error.again': 'Again',
+  'menu.result.error.back': 'Back',
+}
+
+export default en
---
+++ src/app/Result/locales/zh-cn.ts
@@ -0,0 +1,19 @@
+const zh: {
+  [k: string]: string
+} = {
+  'menu.result': '结果页',
+
+  'menu.result.success': '成功页',
+  'menu.result.success.title': '成功信息',
+  'menu.result.success.subTitle': '这是一个成功的描述',
+  'menu.result.success.again': '重试',
+  'menu.result.success.back': '返回',
+
+  'menu.result.error': '失败',
+  'menu.result.error.title': '失败信息',
+  'menu.result.error.subTitle': '发生了一些错误。请重试。',
+  'menu.result.error.again': '重试',
+  'menu.result.error.back': '返回',
+}
+
+export default zh
