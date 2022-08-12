51fbc801bb1abfdf8b98e23b01e8b0f9096531cb
yaogengzhu <455947455@qq.com>
2022-08-12 13:18:16 +0800

    fix: 优化性能监控

--- template/js/index.js
+++ template/js/index.js
@@ -2,16 +2,27 @@
  * @Author: yaogeng.zhu
  * @Date: 2021-10-08 10:17:01
  * @Last Modified by: yaogeng.zhu
- * @Last Modified time: 2022-08-12 13:04:53
+ * @Last Modified time: 2022-08-12 13:17:24
  *  存放一些其他的js脚本
  */
-console.log("%c%s","color: red; background: yellow; font-size: 24px;","数据埋点！");
+console.log('%c%s', 'color: red; background: yellow; font-size: 24px;', '数据埋点！')
 
 function buriedPoint() {
-    const timingInfo = window.performance.timing
-    const {domLoading, fetchStart } = timingInfo
-    const time = domLoading - fetchStart
-    console.log(time + 'ms', '白屏时间')
-    console.log(timingInfo, '时间统计数据')
+  const timingInfo = window.performance.timing
+  const { domLoading, fetchStart } = timingInfo
+  const time = domLoading - fetchStart
+  console.log(time + 'ms', '白屏时间')
+  console.log(timingInfo, '时间统计数据')
 }
-buriedPoint()
+
+window.onload = function () {
+  buriedPoint()
+
+  window.addEventListener('hashchange', buriedPoint)
+  history.pushState = new Proxy(history.pushState, {
+    apply: function (target, thisBinding, args) {
+      buriedPoint()
+      return target.apply(thisBinding, args)
+    },
+  })
+}
